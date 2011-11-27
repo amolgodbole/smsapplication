5304301e3615a91d6d15bd6b1911ba1796d619ee
akshayk1987@gmail.com <akshayk1987@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-27 02:29:38 +0000

    StockSplit modified

---  smsapplication/SMSApplication/src/StockSplit.java
+++  smsapplication/SMSApplication/src/StockSplit.java
@@ -1,4 +1,27 @@
 
-public class StockSplit {
+public class StockSplit extends StockDecorator{
+
+	public StockSplit(Stock s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+	
+	void processSplitStock()
+	{
+		
+		
+	}
+	
+	void updateResult()
+	{
+		
+	}
+	
+	public void display()
+	{
+		super.display();
+	}
+	
+	
 
 }
