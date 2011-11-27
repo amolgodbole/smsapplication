2afe4c6baa588fd4ffa4053ac290a5ef79d8a623
akshayk1987@gmail.com <akshayk1987@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-27 04:03:12 +0000

    OrderImplementor updated

---  smsapplication/SMSApplication/src/OrderImplementor1.java
+++  smsapplication/SMSApplication/src/OrderImplementor1.java
@@ -1,8 +1,30 @@
 
 
-public class OrderImplementor1 {
+public class OrderImplementor1 extends MatchedType{
 
-	//processPartialOrder(buyOrderID,sellOrderID,buyLot,sellLot,buyMatch,sellMatch);
-	//processCompleteOrder(buyOrderID,sellOrderID);
+	private int _buyOrderID, _sellOrderID,_sellLot,_buyLot,_buyMatch,_sellMatch;
 	
-}
+	public OrderImplementor1(ProcessOrderImplementor imp,int buyOrderID,int sellOrderID,int sellLot, int buyLot,int buyMatch,int sellMatch)
+	{
+		super(imp);
+		_buyOrderID=buyOrderID;
+		_sellOrderID=sellOrderID;
+		_buyLot=buyLot;
+		_sellLot=sellLot;
+		_buyMatch=buyMatch;
+		_sellMatch=sellMatch;
+	}
+	
+	
+	
+	@Override
+	public void processOrder() {
+		processPartialOrder(_buyOrderID,_sellOrderID,_buyLot,_sellLot,_buyMatch,_sellMatch);
+	}
+
+	
+		
+	}
+
+	
+
