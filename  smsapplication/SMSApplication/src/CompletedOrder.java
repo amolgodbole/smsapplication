ded6d756cebc60b4f9107ec7ed4db704c86ac728
akshayk1987@gmail.com <akshayk1987@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-27 04:02:33 +0000

    CompleteOrder updated

---  smsapplication/SMSApplication/src/CompletedOrder.java
+++  smsapplication/SMSApplication/src/CompletedOrder.java
@@ -1,9 +1,22 @@
 
-public class CompletedOrder {
+public class CompletedOrder extends ProcessOrderImplementor{
 
 	int buyOrderID;
 	int sellOrderID;
 	
-	//void processOrder(buyOrderID,sellOrderID){};
+
+
+	@Override
+	public boolean processPartialOrder(int buyOrderID, int sellOrderID,
+			int buyLot, int sellLot, int buyMatch, int sellMatch) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean processCompleteOrder(int buyOrderID, int sellOrderID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
