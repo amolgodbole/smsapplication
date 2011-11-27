dd314a8700bb7c09c9d662ac0532c6d8b13623e0
akshayk1987@gmail.com <akshayk1987@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-27 02:29:24 +0000

    StockDecorator modified

--- trunk/ smsapplication/SMSApplication/src/StockDecorator.java
+++ trunk/ smsapplication/SMSApplication/src/StockDecorator.java
@@ -1,4 +1,15 @@
 
-public interface StockDecorator {
+public abstract class StockDecorator {
+
+	protected Stock stock;
+	
+	public StockDecorator(Stock s)
+	{
+		stock=s;
+	}
+
+	public void display() {
+	stock.display();
+	}
 
 }
