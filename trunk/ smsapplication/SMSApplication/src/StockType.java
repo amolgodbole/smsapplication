a2d9bf257ba18c95a8461d979829eb5ac4b1b1b2
akshayk1987@gmail.com <akshayk1987@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-27 02:30:00 +0000

    StockType modified

--- trunk/ smsapplication/SMSApplication/src/StockType.java
+++ trunk/ smsapplication/SMSApplication/src/StockType.java
@@ -1,4 +1,27 @@
 
-public class StockType {
+public class StockType extends Stock{
 
+	
+	public void assignStockID() {
+		
+	
+	}
+	
+	
+	@Override
+	public void getStockListing() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
 }
