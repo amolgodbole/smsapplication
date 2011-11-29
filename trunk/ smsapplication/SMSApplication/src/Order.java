9d5513ff0331c58186159156b547f58a0e2e73c5
amolgodbole87@gmail.com <amolgodbole87@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-29 08:58:12 +0000

    Synchronized

--- trunk/ smsapplication/SMSApplication/src/Order.java
+++ trunk/ smsapplication/SMSApplication/src/Order.java
@@ -1,6 +1,67 @@
 
-public class Order implements OrderTMAAbstractClass{
+
+public class Order 
+{
+	
+	String 		strOrderID;
+	boolean 	partialOrderFlag;
+	Account 	account;
+	String 		orderType;//should be set to MutualFund or Should be set to Stocks
+	
+	
+	public String getStrOrderID() {
+		return strOrderID;
+	}
+
+
+
+	public void setStrOrderID(String strOrderID) {
+		this.strOrderID = strOrderID;
+	}
+
+
+
+	public boolean isPartialOrderFlag() {
+		return partialOrderFlag;
+	}
+
+
+
+	public void setPartialOrderFlag(boolean partialOrderFlag) {
+		this.partialOrderFlag = partialOrderFlag;
+	}
+	
+	
+
+
+
+	public Account getAccount() {
+		return account;
+	}
+
+
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+
+
+	public String getOrderType() {
+		return orderType;
+	}
+
+
+
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+
+}
+
+/*public class Order implements OrderTMAAbstractClass{
+
 	void testorder(){
 		System.out.println("Inside order");
 	}
-}
+}*/
