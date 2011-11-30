4fdf87fbfbe6622aca2d47a9caef2e86c24671b5
kmphadnis@gmail.com <kmphadnis@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-30 06:37:39 +0000

    facade functionality by kaustubh

--- trunk/ smsapplication/SMSApplication/src/Order.java
+++ trunk/ smsapplication/SMSApplication/src/Order.java
@@ -22,35 +22,38 @@ public class Order
 	OrderBean order = new OrderBean();
 	
 	void orderOperation(){
-		
-		System.out.println("Enter your choice : ");
-		System.out.println("1. Create Order");
-		System.out.println("2. Update Order");
-		System.out.println("3. Delete Order");
-		System.out.println("4. View Orders");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String option = br.readLine();
-			if(option.equals("1")){
-				order.setStrOrderID("ORD-01-STK");
-				order.setOrderType("STOCK");
-				order.setPartialOrderFlag(true);
-				order.setInvestorID(this.investor.getInvestorID());
-				
-				investor.orderList.add(order);
-				System.out.println("*************" +investor.getInvestorID());
-				
-				System.out.println("Order places is : " +order.getStrOrderID());
-			}
-			if(option.equals("2")) order.updateOrder();
-			if(option.equals("3")) order.deleteOrder();
-			if(option.equals("4")) order.viewOrders(investor.orderList);
+		while(true) {
 			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				System.out.println("Enter your choice : ");
+				System.out.println("1. Create Order");
+				System.out.println("2. Update Order");
+				System.out.println("3. Delete Order");
+				System.out.println("4. View Orders");
+				System.out.println("5. Exit");
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				try {
+					String option = br.readLine();
+					if(option.equals("1")){
+						order.setStrOrderID("ORD-01-STK");
+						order.setOrderType("STOCK");
+						order.setPartialOrderFlag(true);
+						order.setInvestorID(this.investor.getInvestorID());
+						
+						investor.orderList.add(order);
+						System.out.println("*************" +investor.getInvestorID());
+						
+						System.out.println("Order places is : " +order.getStrOrderID());
+					}
+					if(option.equals("2")) order.updateOrder();
+					if(option.equals("3")) order.deleteOrder();
+					if(option.equals("4")) order.viewOrders(investor.orderList);
+					if(option.equals("5")) break;
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
 		}
-		
-		
 	}
 }
