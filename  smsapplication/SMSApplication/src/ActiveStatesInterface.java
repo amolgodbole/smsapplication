11408993883ecda4e7e44f0cf7490a6c12c97fa7
amolgodbole87@gmail.com <amolgodbole87@gmail.com@46cb3160-1537-5924-73a3-1d88b6195d5c>
2011-11-30 22:11:55 +0000

    Delete Strategy for Completed Order

---  smsapplication/SMSApplication/src/ActiveStatesInterface.java
+++  smsapplication/SMSApplication/src/ActiveStatesInterface.java
@@ -6,8 +6,6 @@ public interface ActiveStatesInterface
 	String updateResult();
 	
 	String processWaitingOrder(OrderBean order, StockOrderInterface stockOrderInterface);
-	//String processMatchingOrder(OrderBean order);
-	//String processMatchedOrder(OrderBean order);
 	String processMatchedOrder(OrderBean order, StockOrderInterface stockOrder);
 	String processMatchingOrder(OrderBean order,StockOrderInterface stockOrderInterface);
 
